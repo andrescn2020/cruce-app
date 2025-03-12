88931b1ff1012aad107076e82f4cec9ed6394fa4
andrescn2020 <andrescn_2022@outlook.com>
2025-03-12 20:24:13 -0300

    Fix bugs

--- nuevas_tasas.py
+++ nuevas_tasas.py
@@ -274,6 +274,6 @@ def index():
 
 
 if __name__ == "__main__":
-    if not os.path.exists(app.config["UPLOAD_FOLDER"]):
-        os.makedirs(app.config["UPLOAD_FOLDER"])
+    # if not os.path.exists(app.config["UPLOAD_FOLDER"]):
+    #     os.makedirs(app.config["UPLOAD_FOLDER"])
     app.run(debug=True, host="0.0.0.0", port=os.getenv("PORT", default=5000))
